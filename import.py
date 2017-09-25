b917eaf04b715803a7c12ef2fb7e9cf0e99c1642
xiao.yuxin.2012@vjc.sg <xiao.yuxin.2012@vjc.sg>
2017-09-25 20:48:10 +0800

    add cql for creating tables

--- import.py
+++ import.py
@@ -70,7 +70,104 @@ def create_column_families(current_session=session, parameters={}):
             "PRIMARY KEY ((w_id), d_id, c_id) "
         "); "
     ).format(**default_params)
+    cql_create_warehouse = (
+        "CREATE TABLE {keyspace}.warehouse( "
+            "w_id       INT, "
+            "w_name     TEXT, "
+            "w_street   TEXT, "
+            "w_city     TEXT, "
+            "w_state    TEXT, "
+            "w_zip      TEXT, "
+            "w_tax      DOUBLE, "
+            "w_ytd      DOUBLE, "
+            "PRIMARY KEY (w_id) "
+        "); "
+        ).format(**default_params)
+    cql_create_orderline = (
+        "CREATE TABLE {keyspace}.orderline( "
+            "w_id               INT, "
+            "d_id               TEXT, "
+            "o_id               TEXT, "
+            "ol_number          TEXT, "
+            "ol_i_id            TEXT, "
+            "ol_i_name          TEXT, "
+            "ol_delivery_d      TIMESTAMP, "
+            "ol_amount          DOUBLE, "
+            "ol_supply_w_id     INT, "
+            "ol_quantity        INT, "
+            "ol_dist_info       TEXT, "
+            "PRIMARY KEY ((w_id), d_id, o_id, ol_number) "
+        "); "
+        ).format(**default_params)
+    cql_create_district = (
+        "CREATE TABLE {keyspace}.district( "
+            "w_id                       INT, "
+            "d_id                       INT, "
+            "d_name                     TEXT, "
+            "d_street                   TEXT, "
+            "d_city                     TEXT, "
+            "d_state                    TEXT, "
+            "d_zip                      TEXT, "
+            "d_tax                      DOUBLE, "
+            "d_ytd                      DOUBLE, "
+            "d_next_o_id                INT, "
+            "last_unfulfilled_order     INT, "
+            "PRIMARY KEY ((w_id), d_id) "
+        "); "
+        ).format(**default_params)
+    cql_create_order = (
+        "CREATE TABLE {keyspace}.order( "
+            "w_id                       INT, "
+            "d_id                       INT, "
+            "o_id                     INT, "
+            "c_id                   INT, "
+            "o_carrier_id                     INT, "
+            "o_ol_cnt                    INT, "
+            "o_all_local                      INT, "
+            "o_entry_d                      TIMESTAMP, "
+            "c_first                      TEXT, "
+            "c_middle                TEXT, "
+            "c_last     TEXT, "
+            "popular_item_id     INT, "
+            "popular_item_name     TEXT, "
+            "popular_item_qty     INT, "
+            "ordered_items     SET<INT>, "
+            "PRIMARY KEY ((w_id), d_id,o_id) "
+        "); "
+        ).format(**default_params)
+    cql_create_stockbywarehouse = (
+        "CREATE TABLE {keyspace}.stock_by_warehouse( "
+            "w_id                       INT, "
+            "i_id                       INT, "
+            "i_name                     TEXT, "
+            "i_price                   DOUBLE, "
+            "i_im_id                     INT, "
+            "i_data                    TEXT, "
+            "s_quantity                      INT, "
+            "s_ytd                      INT, "
+            "s_order_cnt                      INT, "
+            "s_remote_cnt                INT, "
+            "s_dist_info     TEXT, "
+            "s_data     TEXT, "
+            "PRIMARY KEY ((w_id), i_id) "
+        "); "
+        ).format(**default_params)
+    cql_create_customerbybalance = (
+        "CREATE MATERIALISED VIEW {keyspace}.customer_by_warehouse AS"
+            "SELECT * FROM {keyspace}.customer "
+            "WHERE w_id IS NOT NULL and d_id IS NOT NULL and "
+                "c_id IS NOT NULL and c_balance IS NOT NULL "
+            "PRIMARY KEY ((w_id), d_id, c_id, c_balance) "
+        "; "
+        ).format(**default_params)
     current_session.execute(cql_create_customer)
+    current_session.execute(cql_create_warehouse)
+    current_session.execute(cql_create_orderline)
+    current_session.execute(cql_create_district)
+    current_session.execute(cql_create_order)
+    current_session.execute(cql_create_district)
+    current_session.execute(cql_create_stockbywarehouse)
+    current_session.execute(cql_create_customerbybalance)
 
 
 def loading_data(current_session=session):
