71af358e20356e6f5d8b8af5f54513b487f1a003
BigWhiteDog <legend.z@foxmail.com>
2017-10-04 20:40:03 +0800

    transaction5 and act_test

--- act_test.py
+++ act_test.py
@@ -7,29 +7,36 @@ import subprocess
 def act_test(act_file):
 	f=open(act_file)
 	line = f.readline()
-	order_list=[]
-	while line:"O, S, I, or T""
+	
+	while line:
 		para=line.strip().split(",")
 		if para[0]=="N":
-			pass
+			num_item=0
+			new_order_c_id=int(para[1])
+			new_order_w_id=int(para[2])
+			new_order_d_id=int(para[3])
+			new_order_m=int(para[4])
+			new_order_line=list()
+
 		elif para[0]=="P":
-			pass
+			payment_transaction(c_w_id=int(para[1]), c_d_id=int(para[2]), c_id=int(para[3]), payment, current_session=session)
 		elif para[0]=="D":
-			pass
+			txn3(current_session=session, w_id=int(para[1]), carrier_id=int(para[2]))
 		elif para[0]=="O":
-			pass
+			txn4(current_session=session, c_w_id=int(para[1]), c_d_id=int(para[2]), c_id=int(para[3]))
 		elif para[0]=="S":
-			pass
+			stock_level_transaction(w_id=int(para[1]),d_id=int(para[2]),T=int(para[3]), L=int(para[4]), current_session=session)
 		elif para[0]=="I":
-			pass
+			popular_item_transaction(i="I", w_id=int(para[1]), d_id=int(para[2]),L=int(para[3]), current_session=session)
 		elif para[0]=="T":
-			pass
+			top_balance_transaction(current_session=session)
 		else: #order line in the order
-			pass
+			new_order_line.append([int(para[0]),int(para[1]),int(para[2])])
+			num_item += 1
+			if num_item == new_order_m:
+				new_order_transaction(c_id=new_order_c_id, w_id=new_order_w_id, d_id=new_order_d_id,M=new_order_m, items=new_order_line, current_session=session)
 
 		line = f.readline()
-		
-
 	f.close()
 
 if __name__ == '__main__':
--- transactions.py
+++ transactions.py
@@ -289,7 +289,40 @@ def payment_transaction(c_w_id, c_d_id, c_id, payment, current_session=session):
         'payment': payment
     }
     return output
+
+# Current WIP - Not proven to work
+def stock_level_transaction(w_id, d_id,T, L, current_session=session):
+    parameters = {
+        "w_id": w_id,
+        "d_id": d_id,
+        "T": T,
+        "l": L
+    }
+    cql_select_order = (
+        "SELECT w_id, d_id, o_id,orderd_items"
+        "FROM orders "
+        "WHERE w_id = %(w_id)s AND d_id = %(d_id)s "
+        "ORDER BY o_id DESC "
+        "LIMIT %(l)s"
+    )
+    rows = current_session.execute(cql_select_order, parameters=parameters)
+    number_of_entries = len(rows)
+    all_item_id = set()
+    for row in rows:
+        all_item_id = all_item_id | row.orderd_items
+    parameters["all_item_id"]=all_item_id
+    cql_select_order = (
+        "SELECT w_id, i_id, i_name"
+        "FROM stockbywarehouse "
+        "WHERE w_id = %(w_id)s AND i_id IN %(d_id)s AND s_quantitiy < %(T)s"
+    )
+    rows = current_session.execute(cql_select_order, parameters=parameters)
     
+    #output = list(row.i_name for row in rows)
+    #output = [item, float(item_count) / number_of_entries for item, item_count in zip(distinct_popular_item, raw_count)]
+    # TODO: Process rows to output json
+
+
 # Current WIP - Not proven to work
 def popular_item_transaction(i, w_id, d_id, L, current_session=session):
     parameters = {
@@ -299,7 +332,7 @@ def popular_item_transaction(i, w_id, d_id, L, current_session=session):
         "l": L
     }
     cql_select_order = (
-        "SELECT w_id, d_id, o_id. o_entry_d. c_first, c_middle, c_last, "
+        "SELECT w_id, d_id, o_id, o_entry_d, c_first, c_middle, c_last, "
         "popular_item_id, popular_item_name, popular_item_qty, ordered_items "
         "FROM orders "
         "WHERE w_id %(w_id)s AND d_id %(d_id)s "
