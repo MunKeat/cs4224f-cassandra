a102673e1ace9b0171c41aba550c7893589b8309
MunKeat <laimunkeat@gmail.com>
2017-10-09 01:40:35 +0800

    Format massage.sh

--- massage.sh
+++ massage.sh
@@ -1,153 +1,120 @@
 #!/usr/bin/env bash
 
-############################################
-# Joins Stock with Item
-# Produce StockByWarehouse Column Family
-#
-# Assumes the following for schema
-#
-# Stock.csv:
-# 1. S_W_ID
-# 2. S_I_ID
-# 3. S_QUANTITY
-# 4. S_YTD
-# 5. S_ORDER_CNT
-# 6. S_REMOTE_CNT
-# 7. S_DIST_01
-# 8. S_DIST_02
-# 9. S_DIST_03
-# 10. S_DIST_04
-# 11. S_DIST_05
-# 12. S_DIST_06
-# 13. S_DIST_07
-# 14. S_DIST_08
-# 15. S_DIST_09
-# 16. S_DIST_10
-# 17. S_DATA
-#
-# Item.csv:
-# 1. I_ID
-# 2. I_NAME
-# 3. I_PRICE
-# 4. I_IM_ID
-# 5. I_DATA
-############################################
-join -a 1 -j1 2 -j2 1 -t ',' \
-     -o 1.1 2.1 2.2 2.3 2.4 2.5 \
-        1.3 1.4 1.5 1.6 1.7 1.8 1.9 1.10 1.11 1.12 1.13 1.14 1.15 1.16 1.17 -e "null"\
-     ./stock.csv ./item.csv > ./cassandra_stockitem.csv
+##########################################
+# SCRIPT: massage.sh
+##########################################
 
-############################################
-# Joins Warehouse with District
-# Produce (Temporary) Warehouse District
-#
-# Assumes the following for schema
-#
-# Warehouse.csv
-# 1. W_ID
-# 2. W_NAME
-# 3. W_STREET_2
-# 4. W_STREET_1
-# 5. W_CITY
-# 6. W_STATE
-# 7. W_ZIP
-# 8. W_TAX
-# 9. W_YTD
-#
-# District.csv:
-# 1. D_W_ID
-# 2. D_ID
-# 3. D_NAME
-# 4. D_STREET_1
-# 5. D_STREET_2
-# 6. D_CITY
-# 7. D_STATE
-# 8. D_ZIP
-# 9. D_TAX
-# 10. D_YTD
-# 11. D_NEXT_O_ID
-############################################
-join -a 1 -j 1 -t ',' \
-     -o 1.1 2.2 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 \
-        2.3 2.4 2.5 2.6 2.7 2.8 2.9 2.10 2.11 -e "null"\
-     ./warehouse.csv ./district.csv > ./temp_warehouse_district.csv
+##########################################
+# FILES AND VARIABLES DEFINITION(S)
+##########################################
+data_dir="$(pwd)/data"
+# Set output=false to silence the program
+output=true
 
-############################################
-# Joins Warehouse-District With Customer
-# Produce Customer Column Family
-#
-# Assumes the following for schema
-#
-# customer.csv:
-# 1. C_W_ID
-# 2. C_D_ID
-# 3. C_ID
-# 4. C_FIRST
-# 5. C_MIDDLE
-# 6. C_LAST
-# 7. C_STREET_1
-# 8. C_STREET_2
-# 9. C_CITY
-# 10. C_STATE
-# 11. C_ZIP
-# 12. C_PHONE
-# 13. C_SINCE
-# 14. C_CREDIT
-# 15. C_CREDIT_LIM
-# 16. C_DISCOUNT
-# 17. C_BALANCE
-# 18. C_YTD_PAYMENT
-# 19. C_PAYMENT_CNT
-# 20. C_DELIVERY_CNT
-# 21. C_DATA
-############################################
-join -a 1 -j 1 -t ',' \
-     -o 1.2 1.3 1.4 2.4 2.5 2.6 2.10 2.12 2.13 2.14 2.18 \
-        1.5 1.6 1.7 1.8 1.9 1.10 1.11 1.12 1.13 1.14 1.15 1.16 1.17 1.18 1.19 1.20 1.21 1.22 -e "null" \
-     <(paste -d',' <(cut -d',' --output-delimiter=- -f1,2 ./customer.csv) ./customer.csv | sort -t',' -k1,1) \
-     <(paste -d',' <(cut -d',' --output-delimiter=- -f1,2 ./temp_warehouse_district.csv) ./temp_warehouse_district.csv | sort -t',' -k1,1) > ./cassandra_customer.csv
+##########################################
+# FUNCTION(S)
+##########################################
+function verify_prerequisites () {
+  if [[ ! -d "${data_dir}" ]]; then
+    # Should not reach here; directory should exist
+    (>&2 echo "Error: ${data_dir} does not exist")
+    exit 1
+  fi
 
-############################################
-# Current work-in-progress
-#
-# Set all other tables with cassandra_ 
-############################################
-declare -a unchanged_file=("district.csv" "warehouse.csv")
+  declare -a csv_files=("customer.csv" "district.csv" "item.csv" "order-line.csv" "order.csv" "stock.csv" "warehouse.csv")
+  for file in "${csv_files[@]}"; do
+    if [[ ! -s "${data_dir}" ]]; then
+      # Should not reach here; csv should exist
+      (>&2 echo "Error: ${data_dir}/"${file}" does not exist, or has a file size of 0")
+      exit 2
+    fi
+  done
+}
 
-for file in "${unchanged_file[@]}"; do
-	cp "$file" cassandra_"$file"
-done
+function create_new_csv () {
+  # Joins Stock with Item
+  # Produce StockByWarehouse Column Family
+  join -a 1 -j1 2 -j2 1 -t ',' \
+       -o 1.1 2.1 2.2 2.3 2.4 2.5 \
+          1.3 1.4 1.5 1.6 1.7 1.8 1.9 1.10 1.11 1.12 1.13 1.14 1.15 1.16 1.17 -e "null"\
+       "${data_dir}/stock.csv" "${data_dir}/item.csv" > "${data_dir}/cassandra_stockitem.csv"
+
+  if [[ ( "${output}" = true ) && ( "$?" -eq 0 ) ]]; then echo "Created: ${data_dir}/cassandra_stockitem.csv"; fi
+
+  # Joins Warehouse with District
+  # Produce (Temporary) Warehouse District
+  join -a 1 -j 1 -t ',' \
+       -o 1.1 2.2 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9 \
+          2.3 2.4 2.5 2.6 2.7 2.8 2.9 2.10 2.11 -e "null"\
+       "${data_dir}/warehouse.csv" "${data_dir}/district.csv" > "${data_dir}/temp_warehouse_district.csv"
+
+  # Joins Warehouse-District With Customer
+  # Produce Customer Column Family
+  join -a 1 -j 1 -t ',' \
+       -o 1.2 1.3 1.4 2.4 2.5 2.6 2.10 2.12 2.13 2.14 2.18 \
+          1.5 1.6 1.7 1.8 1.9 1.10 1.11 1.12 1.13 1.14 1.15 1.16 1.17 1.18 1.19 1.20 1.21 1.22 -e "null" \
+       <(paste -d',' <(cut -d',' --output-delimiter=- -f1,2 "${data_dir}/customer.csv") "${data_dir}/customer.csv" | sort -t',' -k1,1) \
+       <(paste -d',' <(cut -d',' --output-delimiter=- -f1,2 "${data_dir}/temp_warehouse_district.csv") "${data_dir}/temp_warehouse_district.csv" | sort -t',' -k1,1) > "${data_dir}/cassandra_customer.csv"
+
+  if [[ ( "${output}" = true ) && ( "$?" -eq 0 ) ]]; then echo "Created: ${data_dir}/cassandra_customer.csv"; fi
+
+  # Change order table, join Customer
+  join -a 1 -j 1 -t ','\
+    -o 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9\
+     2.5 2.6 2.7 -e "null" \
+     <(paste -d',' <(cut -d',' --output-delimiter=- -f1,2,4 "${data_dir}/order.csv") "${data_dir}/order.csv" | sort -t',' -k1,1) \
+     <(paste -d',' <(cut -d',' --output-delimiter=- -f1,2,3 "${data_dir}/customer.csv") "${data_dir}/customer.csv" | sort -t',' -k1,1) \
+    > "${data_dir}/cassandra_order.csv"
+
+  if [[ ( "${output}" = true ) && ( "$?" -eq 0 ) ]]; then echo "Created: ${data_dir}/cassandra_order.csv"; fi
+
+  # Change order-line, join Item
+  join -a 1 -j1 5 -j2 1 -t ','\
+    -o 1.1 1.2 1.3 1.4 1.5 2.2 1.6 1.7 1.8 1.9 1.10 -e "null"\
+    <(cat "${data_dir}/order-line.csv" | sort -t',' -k5,5) <(cat "${data_dir}/item.csv" |sort -t',' -k1,1)\
+    > "${data_dir}/cassandra_order-line.csv"
+
+  if [[ ( "${output}" = true ) && ( "$?" -eq 0 ) ]]; then echo "Created: ${data_dir}/cassandra_order-line.csv"; fi
 
 
-#######
-# change order table, join Customer
-#####
-join -a 1 -j 1 -t ','\
-	-o 1.2 1.3 1.4 1.5 1.6 1.7 1.8 1.9\
-	 2.5 2.6 2.7 -e "null" \
-	 <(paste -d',' <(cut -d',' --output-delimiter=- -f1,2,4 ./order.csv) ./order.csv | sort -t',' -k1,1) \
-	 <(paste -d',' <(cut -d',' --output-delimiter=- -f1,2,3 ./customer.csv) ./customer.csv | sort -t',' -k1,1) \
-	> cassandra_order.csv
-####
-# change order-line, join Item
-#####
-join -a 1 -j1 5 -j2 1 -t ','\
-	-o 1.1 1.2 1.3 1.4 1.5 2.2 1.6 1.7 1.8 1.9 1.10 -e "null"\
-	<(cat order-line.csv| sort -t',' -k5,5) <(cat item.csv|sort -t',' -k1,1)\
-	> cassandra_order-line.csv
-	
+  # Set all other tables with cassandra_
+  declare -a unchanged_file=("district.csv" "warehouse.csv")
+  for file in "${unchanged_file[@]}"; do
+    cp "${data_dir}/${file}" "${data_dir}cassandra_${file}"
+    if [[ ( "${output}" = true ) && ( "$?" -eq 0 ) ]]; then echo "Created: ${data_dir}/cassandra_${file}"; fi
+  done
 
+  # Cleanup
+  rm "${data_dir}/temp_warehouse_district.csv"
+}
 
-## Append null
+function append_null () {
+  ## Append null
+  sed -i 's/$/,null,null,null/' "${data_dir}/cassandra_customer.csv"
+  sed -i 's/$/,null/' "${data_dir}/cassandra_district.csv"
+  sed -i 's/$/,null,null,null,null/' "${data_dir}/cassandra_order.csv"
+}
 
-sed -i 's/$/,null,null,null/' cassandra_customer.csv
-sed -i 's/$/,null/' cassandra_district.csv
-sed -i 's/$/,null,null,null,null/' cassandra_order.csv
+function extract_id () {
+  awk -F "," '{printf("%d\n", $1) }' "${data_dir}/warehouse.csv" >>"${data_dir}/wid_list.csv"
+  if [[ ( "${output}" = true ) && ( "$?" -eq 0 ) ]]; then echo "Created: ${data_dir}/wid_list.csv"; fi
 
-## Extract w_id and customer_id into csv file
-awk -F "," '{printf("%d\n", $1) }' ./warehouse.csv >>wid_list.csv
-awk -F "," '{print $1 "," $2 "," $3}' ./customer.csv >>cid_list.csv
+  awk -F "," '{print $1 "," $2 "," $3}' "${data_dir}/customer.csv" >>"${data_dir}/cid_list.csv"
+  if [[ ( "${output}" = true ) && ( "$?" -eq 0 ) ]]; then echo "Created: ${data_dir}/cid_list.csv"; fi
+}
 
-# Cleanup
-rm ./temp_warehouse_district.csv
+##########################################
+# MAIN BODY
+##########################################
+function main () {
+  # Ensure that the data directory exist, and that it contains the necessary files
+  verify_prerequisites
+  # Create new CSV using the JOIN command
+  create_new_csv
+  # Append null as placeholder for the newly created csv files
+  append_null
+  # Extract w_id and customer_id into csv file
+  extract_id
+}
 
+main
