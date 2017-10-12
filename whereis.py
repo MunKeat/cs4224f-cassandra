6e2a7c83495ef1d40aefe34488f34c2896194861
MunKeat <laimunkeat@gmail.com>
2017-10-12 15:55:18 +0800

    Remove whereis.py

--- whereis.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-#suppose if you're in virtual env
-a=subprocess.Popen(["whereis","cqlsh"],stdout=subprocess.PIPE)
-b=a.stdout.readline()
-c=b.split(" ")
-#c[1] is the cqlsh in virtual env, c[2] is the real cqlsh
