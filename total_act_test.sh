1dddcc4f9863f67781169d4458ff630b28f144c1
BigWhiteDog <legend.z@foxmail.com>
2017-10-13 01:22:19 +0800

    fix the run script

--- total_act_test.sh
+++ total_act_test.sh
@@ -2,9 +2,9 @@
 #rank $1
 #nc $2
 python dbstate.py > $1-state.txt
-for((i=1;i<=$2;i++))
+for(( i = 1 ; i <= $2 ; i++ ))
 {
-	if((i%$2==$1))
+	if((i % 5 == $1))
 	then
 		python act_test.py < ./xact-files/$i.txt > /dev/null 2> ./xres/$i-res.txt &
 	fi
