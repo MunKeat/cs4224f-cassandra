22483911e3840978c9ffcd09fe896d02bc9af891
BigWhiteDog <legend.z@foxmail.com>
2017-10-12 18:48:49 +0800

    act_test change
    add time and import trans

--- act_test.py
+++ act_test.py
@@ -1,16 +1,15 @@
 from cassandra.cluster import Cluster
 import os
 
-from transactions import new_order_transaction, payment_transaction, stock_level_transaction,delivery_transaction,order_status_transaction, popular_item_transaction, top_balance_transaction
+
+import transactions
+
+import time
 
 
-cluster = Cluster(["192.168.48.244","192.168.48.245","192.168.48.246","192.168.48.247","192.168.48.248"])
-session = cluster.connect("warehouse")
-
-def act_test(act_file):
-	f=open(act_file)
+def act_test(session):
 	line = f.readline()
-
+	num_tran=0
 	while line:
 		para=line.strip().split(",")
 		if para[0]=="N":
@@ -20,33 +19,57 @@ def act_test(act_file):
 			new_order_d_id=int(para[3])
 			new_order_m=int(para[4])
 			new_order_line=list()
-
+			num_tran+=1
 		elif para[0]=="P":
-			payment_transaction(c_w_id=int(para[1]), c_d_id=int(para[2]), c_id=int(para[3]), payment=float(para[4]), current_session=session)
+			transactions.payment_transaction(c_w_id=int(para[1]), c_d_id=int(para[2]), c_id=int(para[3]), payment=float(para[4]), current_session=session)
+			num_tran+=1
 		elif para[0]=="D":
-			delivery_transaction(current_session=session, w_id=int(para[1]), carrier_id=int(para[2]))
+			transactions.delivery_transaction(current_session=session, w_id=int(para[1]), carrier_id=int(para[2]))
+			num_tran+=1
 		elif para[0]=="O":
-			order_status_transaction(current_session=session, c_w_id=int(para[1]), c_d_id=int(para[2]), c_id=int(para[3]))
+			transactions.order_status_transaction(current_session=session, c_w_id=int(para[1]), c_d_id=int(para[2]), c_id=int(para[3]))
+			num_tran+=1
 		elif para[0]=="S":
-			stock_level_transaction(w_id=int(para[1]),d_id=int(para[2]),T=int(para[3]), L=int(para[4]), current_session=session)
+			transactions.stock_level_transaction(w_id=int(para[1]),d_id=int(para[2]),T=int(para[3]), L=int(para[4]), current_session=session)
+			num_tran+=1
 		elif para[0]=="I":
-			popular_item_transaction(i="I", w_id=int(para[1]), d_id=int(para[2]),L=int(para[3]), current_session=session)
+			transactions.popular_item_transaction(i="I", w_id=int(para[1]), d_id=int(para[2]),L=int(para[3]), current_session=session)
+			num_tran+=1
 		elif para[0]=="T":
-			top_balance_transaction(current_session=session)
+			transactions.top_balance_transaction(current_session=session)
+			num_tran+=1
 		else: #order line in the order
 			new_order_line.append([int(para[0]),int(para[1]),int(para[2])])
 			num_item += 1
 			if num_item == new_order_m:
-				new_order_transaction(c_id=new_order_c_id, w_id=new_order_w_id, d_id=new_order_d_id,M=new_order_m, items=new_order_line, current_session=session)
+				transactions.new_order_transaction(c_id=new_order_c_id, w_id=new_order_w_id, d_id=new_order_d_id,M=new_order_m, items=new_order_line, current_session=session)
 
 		line = f.readline()
-	f.close()
+	
+	return num_tran
 
 if __name__ == '__main__':
-	for i in xrange(1,41):
-		act_test("%d.txt" % i)
 
+	current_directory = os.path.dirname(os.path.realpath(__file__))
+	config_path = os.path.join(os.path.sep, current_directory, "config.conf")
+	default_parameters = {}
+	execfile(config_path, default_parameters)
+	
+	cluster = Cluster(contact_points=default_parameters['hosts'])
+	session = cluster.connect("warehouse")
+	
+	num_tran=0
+	before_time=time.time()
+	
+	num_tran=act_test(session)
+	
+	after_time=time.time()
 
+	total_time=after_time-before_time
+
+	sys.stderr.write("Total number of transactions processed:%d\n",num_tran)
+	sys.stderr.write("Total elapsed time for processing the transactions:%f",total_time)
+	sys.stderr.write("Transaction throughput:%f",total_time/(float)num_tran)
 
 
 '''You can use a new cqlsh command, CONSISTENCY, to set the consistency level for queries from the current cqlsh session.
--- total_act_test.py
+++ total_act_test.py
@@ -5,7 +5,7 @@ import thread
 def swim_act(rank,nc,nact):
 	for i in range(1,nact+1):
 		if(i%nc==rank)
-			subprocess.call("python act_test.py < %d.txt" % i,shell=True)
+			subprocess.call("python act_test.py < %d.txt &" % i,shell=True)
 if __name__ == '__main__':
 	rank=int(sys.argv[1])
 	nc=int(sys.argv[2])
