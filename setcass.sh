cdaba7990a86cf45e0b9867dbcd6e520150a40e7
MunKeat <MunKeat@users.noreply.github.com>
2017-10-15 13:20:56 +0800

    Delete setcass.sh

--- setcass.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-wget https://bootstrap.pypa.io/get-pip.py
-python get-pip.py --user
-exploit PATH=$PATH:$HOME/.local/bin
-if [[ ! -d '$HOME/mypylib' ]]; then
-	cd ~
-	mkdir mypylib
-fi
-pip install --prefix=$HOME/mypylib cassandra-driver
